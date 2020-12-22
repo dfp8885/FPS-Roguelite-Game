d677d8fb76adc69574cd0fad58867636292b220c
DennisPalad <dfp8885@rit.edu>
2020-12-21 15:35:45 -1000

    Created a Player Body with Basic Movement and Gravity
    This implements a player with wasd movement and jumping as well as being able to look around properly with a mouse.

---
+++ FPS Roguelite Game/Assets/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0) { velocity.y = -2f; }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right*x + transform.forward*z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        // Equation for a jump {v = sqrt(h*-2*g)}
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        // Equation for a free fall {delta(y) = 0.5g*t^2} so we need to multiplay by time again 
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
