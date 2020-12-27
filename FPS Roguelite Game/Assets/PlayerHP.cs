b7c4c72f03c89e9e557cd45477e6f1d77bcdfece
DennisPalad <dfp8885@rit.edu>
2020-12-26 23:48:05 -1000

    Created a Visualization of the Shield Recharge Delay and Put Limitations on Tmep Invuln
    On screen timer for shield recharge delay has been created.
    Temporary invulnerability can only proc if shields are tken down from being fully recovered.

--- FPS Roguelite Game/Assets/PlayerHP.cs
+++ FPS Roguelite Game/Assets/PlayerHP.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerHP : MonoBehaviour
 {
-    public int maxHealth = 100;
-    public int maxShield = 50;
+    public int maxHealth = 200;
+    public int maxShield = 100;
     
     int currentHealth;
     int currentShield;
@@ -13,50 +13,78 @@ public class PlayerHP : MonoBehaviour
     public HPBar healthBar;
     public HPBar shieldBar;
 
-    bool shieldInvulnUp = true;
-    float shieldInvulnCooldown = 10f;
+    public CooldownTimer shieldCooldown;
+
+    bool recoveredFromFull = true;
     float invulnTime = 1f;
-    float nextShield;
     float invulnActive;
+
+    float shieldRechargeDelay = 5f;
+    int shieldRechargeRate = 1;
+    float shieldRechargeTick = 0.05f;
+    float nextUpdate;
+    float nextRecharge;
     
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         currentHealth = maxHealth;
         currentShield = maxShield;
 
         healthBar.SetMaxHP(maxHealth);
         shieldBar.SetMaxHP(maxShield);
+
+        nextRecharge = Time.time;
+        nextUpdate = Time.time + shieldRechargeTick;
+    }
+
+    void Update(){
+        if (currentShield < maxShield && Time.time > nextRecharge && Time.time > nextUpdate) {
+            RechargeShield(shieldRechargeRate);
+            nextUpdate = Time.time + shieldRechargeTick;
+        }
+
+        if (Input.GetMouseButtonDown(0)) {
+            TakeDamage(50);
+        }
+        shieldCooldown.SetTimer(nextRecharge - Time.time);
     }
 
     public void TakeDamage(int damage) {
         // Check if player has invulnerability currently active
         if (Time.time > invulnActive) {
-            // Check the cooldown of shield invulnerability
-            if (Time.time > nextShield)
-            {
-                shieldInvulnUp = true;
-            }
 
             // Always send damage to shield first
             currentShield -= damage;
             // Check if the shield has no more hit points and that the cooldown for invulnerability is up
-            if (currentShield <= 0 && shieldInvulnUp){
+            if (currentShield <= 0 && recoveredFromFull){
                 currentShield = 0;
                 invulnActive = Time.time + invulnTime;
-                nextShield = Time.time + shieldInvulnCooldown;
-                shieldInvulnUp = false;
+                recoveredFromFull = false;
             }
-            else if (currentShield <= 0 && !shieldInvulnUp) {
+            else if (currentShield <= 0 && !recoveredFromFull) {
                 int tempDamage = Mathf.Abs(currentShield);
                 currentHealth -= tempDamage;
             }
+
+            // Update Recharge Delay
+            nextRecharge = Time.time + shieldRechargeDelay;
 
             // Update HP Bars
             healthBar.SetHP(currentHealth);
             shieldBar.SetHP(currentShield);
         }
+    }
+
+    public void RechargeShield(int amount) {
+        currentShield += amount;
+        shieldBar.SetHP(currentShield);
+        if (currentShield == maxShield) { recoveredFromFull = true; }
+    }
+
+    public void Heal(int amount) {
+        currentHealth += amount;
+        healthBar.SetHP(currentHealth);
     }
 
 
